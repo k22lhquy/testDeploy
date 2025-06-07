2374f7847c45b08a661f664b50c14cc2de4de176
quy le <quyle150802@gmail.com>
2025-06-08 00:07:55 +0700

    test

--- backend/lib/socket.js
+++ backend/lib/socket.js
@@ -5,12 +5,9 @@ import { Server } from "socket.io";
 const app = express();
 const server = http.createServer(app);
 
-// 👇 Thêm domain frontend đã deploy
 const io = new Server(server, {
   cors: {
     origin: ["http://localhost:3000", "https://xx-m8te.onrender.com"],
-    methods: ["GET", "POST"],
-    credentials: true,
   },
 });
 
@@ -25,6 +22,7 @@ io.on("connection", (socket) => {
   const userId = socket.handshake.query.userId;
   if (userId) userSocketMap[userId] = socket.id;
 
+  // io.emit() is used to send events to all the connected clients
   io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
   socket.on("disconnect", () => {
