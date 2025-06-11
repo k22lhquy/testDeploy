9a9ff150b3e954a71ff362b9e4c1d804b4d1ef0d
quy le <quyle150802@gmail.com>
2025-06-11 07:46:18 +0700

    deploy

--- frontend/src/apis/axiosInstance.js
+++ frontend/src/apis/axiosInstance.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const axiosInstance = axios.create({
   // baseURL: "http://localhost:5000",
   baseURL: "https://xx-m8te.onrender.com",
-  timeout: 10000,
+  timeout: 20000,
   headers: {
     "Content-Type": "application/json",
   },
